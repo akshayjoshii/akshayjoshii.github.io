61465cc77fc1d08cbc235f862100bdb178613877
Akshay Joshi <akshayjoshi56@gmail.com>
2020-09-15 08:40:33 +0200

    Update demo.sh

--- academia-hugo/demo.sh
+++ academia-hugo/demo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd academia-hugo/exampleSite
-hugo --themesDir ../../ --i18n-warnings -gc --minify --enableGitInfo --baseURL $URL
+hugo --themesDir ../../ --i18n-warnings --gc --minify --enableGitInfo --baseURL $URL
